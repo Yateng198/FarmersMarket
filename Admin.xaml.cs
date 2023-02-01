e4bf42b0b37e28bcf939e955d1d27e56576b19a5
Yateng Geng <yatenggeng@gmail.com>
2023-01-31 21:52:30 -0500

    Updated error input handlers

--- Admin.xaml.cs
+++ Admin.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace FarmersMarketApp
 {
@@ -31,7 +32,7 @@ namespace FarmersMarketApp
             InitializeComponent();
             //set up and open connection for this whole page, so that using a single connection with the DB
             //Ensure that data are synchronized
-            con = new SqlConnection("Data Source=LAPTOP-DT6BMRBG;Initial Catalog=final;Integrated Security=True");
+            con = new SqlConnection("Data Source=DESKTOP-1AHTENP;Initial Catalog=FarmersMarket;Integrated Security=True;Pooling=False");
             con.Open();
 
         }
@@ -61,10 +62,19 @@ namespace FarmersMarketApp
                     string query = "insert into ProductTable values(@ProductId, @ProductName, @Amount, @Price)";
                     SqlCommand cmd = new SqlCommand(query, con);
                     //Fill up the place holders with user input
-                    cmd.Parameters.AddWithValue("@ProductId", int.Parse(p_id.Text));
-                    cmd.Parameters.AddWithValue("@ProductName", p_name.Text);
-                    cmd.Parameters.AddWithValue("@Amount", int.Parse(p_amount.Text));
-                    cmd.Parameters.AddWithValue("@Price", float.Parse(p_price.Text));
+
+                    //In case of user input errors, pop out message box for error message
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@ProductId", int.Parse(p_id.Text));
+                        cmd.Parameters.AddWithValue("@ProductName", p_name.Text);
+                        cmd.Parameters.AddWithValue("@Amount", int.Parse(p_amount.Text));
+                        cmd.Parameters.AddWithValue("@Price", float.Parse(p_price.Text));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Input error, try again please!");
+                    }
                     //Excute the command
                     cmd.ExecuteNonQuery();
                     string name = p_name.Text.ToString();
@@ -97,25 +107,34 @@ namespace FarmersMarketApp
                     string readerQuery = "Select ProductName, Amount from ProductTable where ProductId = @Id";
                     SqlCommand command = new SqlCommand(query, con);
                     SqlCommand readerCmd = new SqlCommand(readerQuery, con);
-                    command.Parameters.AddWithValue("@Id", int.Parse(p_id.Text));
-                    readerCmd.Parameters.AddWithValue("@Id", int.Parse(p_id.Text));
+
+                    //In case of user input errors, pop out message box for error message
+                    int pro_id = 0;
+                    try
+                    {
+                        pro_id = int.Parse(p_id.Text);
+                       
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Input error, try again please!");
+                    }
+                    command.Parameters.AddWithValue("@Id", pro_id);
+                    readerCmd.Parameters.AddWithValue("@Id", pro_id);
 
 
                     SqlDataReader reader = readerCmd.ExecuteReader();
 
                     string name = "";
                     string amount = "";
-                    while (reader.Read())
+                    if(!reader.HasRows)
+                    {
+                        MessageBox.Show("There is no item in database matchs this ID, please check you input and try again!");
+                    }else if(reader.Read())
                     {
                         name = reader.GetValue(0).ToString();
                         amount = reader.GetValue(1).ToString();
-                    }
-                    if (reader == null)
-                    {
-                        MessageBox.Show("There is no item in database matchs this ID, please check you input and try again!");
-                    }
-                    else
-                    {
+
                         reader.Close();
                         command.ExecuteNonQuery();
                         MessageBox.Show(amount + "kg " + name + " have been successfully deleted from database");
@@ -144,14 +163,41 @@ namespace FarmersMarketApp
                 Application.Current.Dispatcher.Invoke(new Action(() =>
                 {
                     string query = "update ProductTable set Productname = @name, Amount = @amount, Price = @price where ProductId = @Id";
+                    string readerQuery = "Select ProductName, Amount from ProductTable where ProductId = @Id";
                     SqlCommand cmd = new SqlCommand(query, con);
-                    cmd.Parameters.AddWithValue("@Id", int.Parse(p_id.Text));
+                    SqlCommand readerCmd = new SqlCommand(readerQuery, con);
+                    int pro_id = 0, pro_amount = 0, pro_price = 0;
+                    //In case of user input errors, pop out message box for error message
+                    try
+                    {
+                        pro_id = int.Parse(p_id.Text);
+                        pro_amount = int.Parse(p_amount.Text);
+                        pro_price = int.Parse(p_price.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Input error, try again please!");
+                    }
+                    cmd.Parameters.AddWithValue("@Id", pro_id);
                     cmd.Parameters.AddWithValue("@name", p_name.Text);
-                    cmd.Parameters.AddWithValue("@amount", int.Parse(p_amount.Text));
-                    cmd.Parameters.AddWithValue("@price", float.Parse(p_price.Text));
-                    //Execute the command
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show("Data updated into database successfully!");
+                    cmd.Parameters.AddWithValue("@amount", pro_amount);
+                    cmd.Parameters.AddWithValue("@price", pro_price);
+                    
+                    
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (!reader.HasRows)
+                    {
+                        MessageBox.Show("There is no item in database matchs this ID, please check you input and try again!");
+                        reader.Close();
+                    }
+                    else if (reader.Read())
+                    {
+                        reader.Close();
+                        //Execute the command
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data updated into database successfully!");
+                    }
                 }));
                 
 
@@ -178,26 +224,34 @@ namespace FarmersMarketApp
                     string qury = "select ProductName, Amount, Price from ProductTable where ProductId = @Id";
                     SqlCommand cmd = new SqlCommand(qury, con);
                     //Fill up the product id with user input
-                    cmd.Parameters.AddWithValue("@Id", int.Parse(p_id.Text));
+                    int pro_id = 0;
+                    try
+                    {
+                         pro_id = (int.Parse(p_id.Text));
+                    }catch(Exception ex) { 
+                        MessageBox.Show("Input error, try again please!");
+                    }
+                    
+
+                    cmd.Parameters.AddWithValue("@Id",pro_id );
                     SqlDataReader reader = cmd.ExecuteReader();
                     //If there's no object matching with this id, will pop this msg box with error msg
-                    if (reader == null)
+                    if (!reader.HasRows)
                     {
                         MessageBox.Show("There is no item in database matchs this ID, please check you input and try again!");
                     }
-                    else
-                    {
-                        while (reader.Read())
+                    else if(reader.Read())
                         {
                             //Fill up the corresponding textbox with this specific product information
                             p_name.Text = reader.GetValue(0).ToString();
                             p_amount.Text = reader.GetValue(1).ToString();
                             p_price.Text = reader.GetValue(2).ToString();
-                        }
+
+                        MessageBox.Show("With ID " + p_id.Text.ToString() + ", we found " + p_amount.Text + "kg " + p_name.Text + " with price " + p_price.Text + " CAD/kg in database");
                     }
 
                     reader.Close();
-                    MessageBox.Show("With ID " + p_id.Text.ToString() + ", we found " + p_amount.Text + "kg " + p_name.Text + " with price " + p_price.Text + " CAD/kg in database");
+                    
                 }));
                 
             }
--- obj/Debug/net6.0-windows/App.g.cs
+++ obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace FarmersMarketApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace FarmersMarketApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             FarmersMarketApp.App app = new FarmersMarketApp.App();
             app.InitializeComponent();
